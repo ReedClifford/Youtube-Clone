be9a026b0fc4136ccdf1029341e9b97f2dfa206c
Shichi <hakubaaaa@gmail.com>
2022-12-09 19:50:39 +0800

    fixed the search bar bug

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import useYoutubeData from "../hooks/useYoutubeData";
 const SearchBar = () => {
   const { setInput } = useYoutubeData();
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
   const onSubmit = (e) => {
     e.preventDefault();
     setInput(search);
+    navigate("/");
   };
   return (
     <div className="relative">

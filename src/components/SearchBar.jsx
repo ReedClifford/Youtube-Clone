deb816924b6eab8d0462524754db6883dcd14607
Shichi <hakubaaaa@gmail.com>
2022-12-09 17:45:51 +0800

    functionalities all done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import useYoutubeData from "../hooks/useYoutubeData";
 const SearchBar = () => {
+  const { setInput } = useYoutubeData();
+  const [search, setSearch] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setInput(search);
+  };
   return (
     <div className="relative">
-      <FaSearch className="text-neutral-700 absolute right-3 top-2 z-20" />
-      <input className="searchBar" placeholder="Search" />
+      <form onSubmit={onSubmit}>
+        <FaSearch className="text-neutral-700 absolute right-3 top-2 z-20" />
+        <input
+          className="searchBar"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 };
